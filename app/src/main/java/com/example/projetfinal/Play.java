a6f72a1b70e94eb353d7ce83d7d028578fa7dff8
Joseph Bombardier <joseph@jopem.com>
2022-12-02 13:44:02 -0500

    play v1

--- app/src/main/java/com/example/projetfinal/Play.java
+++ app/src/main/java/com/example/projetfinal/Play.java
@@ -3,13 +3,22 @@ package com.example.projetfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Play extends AppCompatActivity {
+    int QuiJou = 1; // 1 ou -1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-        
+
+        Button btn11 = (Button) findViewById(R.id.button11);
+
+
+
+
+
+
     }
 }
