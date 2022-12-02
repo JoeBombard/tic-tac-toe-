8c2a8650930e34b25c90f59f62ee96ddbf9e9683
Joseph Bombardier <joseph@jopem.com>
2022-12-01 19:38:18 -0500

    layout main activity

---
+++ app/src/main/java/com/example/projetfinal/Play.java
@@ -0,0 +1,15 @@
+package com.example.projetfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Play extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        
+    }
+}
